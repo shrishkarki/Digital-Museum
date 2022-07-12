fdb51f7bdfad39ac2e4053484213163e80e346f4
unknown <shrishkarki6669@gmail.com>
2022-07-12 18:52:31 +0545

    kina yasto lyaang lyaang

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tw-elements';
 import { IndexContextProvider } from './Components/ContextAPi/IndexContext';
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 
-const APP=(
-  <IndexContextProvider>
-      <App />
-  </IndexContextProvider>
-);
- 
-const rootElement = document.getElementById("root");
-if (rootElement?.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
- 
+// const APP=(
 //   <IndexContextProvider>
-//  <App />
+//       <App />
 //   </IndexContextProvider>
-
 // );
+ 
+// const rootElement = document.getElementById("root");
+// if (rootElement?.hasChildNodes()) {
+//   hydrate(APP, rootElement);
+// } else {
+//   render(APP, rootElement);
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+  <IndexContextProvider>
+ <App />
+  </IndexContextProvider>
+
+);
 
 
 reportWebVitals();
